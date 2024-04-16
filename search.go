687b0ecf19a3720f22348d380003b2df23ca7af2
Viet Hung <viethung0823@gmail.com>
2024-04-16 23:11:50 +0700

    update path

--- search.go
+++ search.go
@@ -27,7 +27,7 @@ func parseCSV() map[string]string {
 	var err error
 
 	// Load file
-	f, err := os.Open("websites.csv")
+	f, err := os.Open("/Users/viethung/working_space/GitHub/alfred-web-searches/workflow/websites.csv")
 	if err != nil {
 		panic(err)
 	}
